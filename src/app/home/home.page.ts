a7207f8a658767ba8d6b6400e350c30af83a6693
Rutger van Eerd <rveerd@vanboxtel.nl>
2019-04-18 10:23:04 +0200

    Added infinite scroll.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  items: string[] = [];
+
+  ngOnInit() {
+    this.loadMore();
+  }
+
+  loadMore(event?) {
+    setTimeout(_ => {
+      this.items.push('Apples', 'Bananas', 'Cherries');
+      if (event)
+        event.target.complete();
+    }, 500);
+  }
 }
